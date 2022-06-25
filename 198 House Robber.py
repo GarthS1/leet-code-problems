2d071a8b7e95b368fdac53d8b4b3928f388fbcc6
Garth Slaney <45348251+GarthS1@users.noreply.github.com>
2022-06-25 09:42:50 -0400

    Create 198 House Robber.py

---
+++ 198 House Robber.py
@@ -0,0 +1,13 @@
+#https://leetcode.com/problems/house-robber/
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        cur, t_0 = 0, nums[0]
+        
+        t_1 = nums[1] if len(nums) > 1 else 0
+        t_2 = nums[2] + t_0 if len(nums) > 2 else 0
+        
+        for i in range(3, len(nums)):
+            cur = nums[i] + max(t_0,t_1)
+            t_0, t_1, t_2 = t_1, t_2, cur
+        
+        return max(t_0,t_1,t_2)
